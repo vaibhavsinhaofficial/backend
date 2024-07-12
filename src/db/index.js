455208aec566dbda3baa6166d2c194afa473893e
vaibhavsinha <vaibhavsinha840@gmail.com>
2024-07-12 15:09:59 +0530

    DB updated

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,8 @@ const connectDB = async() =>{
     try {
 
       const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`\n MongoDB connected!! DB HOST : ${connectionInstance.connection.host}`);
+        console.log(`\n MongoDB connected ✅ !! DB HOST : ${connectionInstance.connection.host}`);
+        
         
     } catch (error) {
         console.log("MONGODB Connection FAILED ", error);
